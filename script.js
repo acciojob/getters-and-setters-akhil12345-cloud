22a357a9270545916090357ba1c4f55c59673e81
akhilbns <74220096+akhil12345-cloud@users.noreply.github.com>
2026-04-03 22:14:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this._name=name,
+		this._age=age
+	}
+		get name(){
+			return this._name
+		}
+		set age(newAge){
+			this._age=newAge
+		}
+	
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(name,age,study){
+		super(name,age)
+		this._study=study
+	}
+	study(){
+		return this.name+" is studying"
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	constructor(name,age,teach){
+		super(name,age)
+		this._teach=teach
+	}
+	teach(){
+		return this.name+" is teaching"
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
